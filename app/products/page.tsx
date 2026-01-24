baae425bb1c2179f08f89699c23c2298bd5b5b3f
Yashi Dubey <yashidubey2739@gmail.com>
2026-01-24 21:16:49 +0530

    fix: make products page dynamic to avoid oversized ISR payload

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+export const dynamic = "force-dynamic";
+
 import Image from "next/image";
 import { connectDB } from "@/lib/db";
 import Product from "@/src/models/Product";
