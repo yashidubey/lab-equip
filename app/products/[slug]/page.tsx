abed1860ae0d1a5ee5e9f2d34bb27c41c1f1c6e8
Yashi Dubey <yashidubey2739@gmail.com>
2026-07-24 15:28:44 +0530

    Remove offers object from product schema

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -119,34 +119,22 @@ export default async function ProductDetailPage({
     : [];
 
   const productSchema = {
-    "@context": "https://schema.org",
-    "@type": "Product",
+  "@context": "https://schema.org",
+  "@type": "Product",
 
-    name: product.name,
+  name: product.name,
 
-    description,
+  description,
 
-    image: image ? [image] : [],
+  image: image ? [image] : [],
 
-    brand: {
-      "@type": "Brand",
-      name: "Labzen",
-    },
+  brand: {
+    "@type": "Brand",
+    name: "Labzen",
+  },
 
-    url: `https://www.labzen.in/products/${product.slug}`,
-
-    offers: {
-      "@type": "Offer",
-      url: `https://www.labzen.in/products/${product.slug}`,
-      priceCurrency: "INR",
-      availability: "https://schema.org/InStock",
-      seller: {
-        "@type": "Organization",
-        name: "Labzen",
-      },
-    },
-  };
-
+  url: `https://www.labzen.in/products/${product.slug}`,
+};
   return (
     <>
       <script
